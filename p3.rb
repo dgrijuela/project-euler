13039fcfb35544b24993c7d0754ee47a56ca9bd4
Daniel Grijuela <danielgrijuela@gmail.com>
2015-01-04 00:43:48 +0100

    Optimize p3

--- p3.rb
+++ p3.rb
@@ -2,18 +2,6 @@
 # 
 # What is the largest prime factor of the number 600851475143 ?
 
-#def is_prime(x)
-#  if (x == 0) || (x == 1)
-#    return false
-#  elsif (x == 2) || (x == 3) || (x == 5)
-#    return true
-#  elsif (x % 2 != 0) && (x % 3 != 0) && (x % 5 != 0)
-#    return true
-#  else
-#    return false
-#  end
-#end
-
 def is_prime(x)
   if x < 2
     return false
@@ -28,20 +16,6 @@ def is_prime(x)
     return true
   end
 end
-
-#def is_prime(x)
-#  if x <= 3
-#    return x >= 2
-#  else
-#    i = 2
-#    while i < x
-#      if x % i == 0
-#        return false
-#      end
-#      i += 1
-#    end
-#  end
-#end
 
 number = 600851475143
 
